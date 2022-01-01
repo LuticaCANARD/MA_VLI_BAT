3b0d4cf420d710d2a9c3ac2c359ff13ca0dbcdfd
LuticaCANARD <presan100@gmail.com>
2022-01-01 11:57:32 +0900

    데미지 입력

--- ConsoleApp1/ConsoleApp1/weapon_to_atp.cs
+++ ConsoleApp1/ConsoleApp1/weapon_to_atp.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
     {
         public int[] String_to_ATP(string atk_weapn , string def_weapn,string atk_scope)
         {
+            
             bool atk_nearwep = atk_weapn == "식칼" || atk_weapn == "창" || atk_weapn == "야구배트";
             bool atk_handgun = atk_weapn == "권총" || atk_weapn == "리볼버" || atk_weapn == "자동권총";
             bool atk_civilgun = atk_weapn == "산탄총" || atk_weapn == "볼트액션 소총" || atk_weapn == "민수용 반자동소총";
@@ -21,7 +22,49 @@ namespace ConsoleApp1
             bool def_millitagun = def_weapn == "기관단총" || def_weapn == "자동소총" || def_weapn == "지정사수소총";
 
             bool have_scope = atk_scope != "없음";
-            
+            if (atk_nearwep & def_nearwep)
+            {
+                if(atk_weapn== "야구배트"& def_weapn == "식칼")
+                {
+                    Random r = new Random();
+                    int min_damage = 15;
+                    int max_damage = 25;
+                    int delta = max_damage - min_damage;
+                    int damage = r.Next(delta) + min_damage;
+                    int[] via = { 0, damage, damage };
+                    return via;
+                }
+                else if ( atk_weapn =="창"& def_weapn != "창")
+                {
+                    Random r = new Random();
+                    int min_damage = 15;
+                    int max_damage = 25;
+                    int delta = max_damage - min_damage;
+                    int damage = r.Next(delta) + min_damage;
+                    int[] via = { 0, damage, damage };
+                    return via;
+                }else if(atk_weapn == "식칼")
+                {
+                    Random r = new Random();
+                    int min_damage = 5;
+                    int max_damage = 10;
+                    int delta = max_damage - min_damage;
+                    int damage = r.Next(delta) + min_damage;
+                    int[] via = { 0, 0, damage };
+                    return via;
+                }
+                else
+                {
+                    Random r = new Random();
+                    int min_damage = 15;
+                    int max_damage = 25;
+                    int delta = max_damage - min_damage;
+                    int damage = r.Next(delta) + min_damage;
+                    int[] via = { 0, 0, damage };
+                    return via;
+                }
+
+            }
 
         }
     }
